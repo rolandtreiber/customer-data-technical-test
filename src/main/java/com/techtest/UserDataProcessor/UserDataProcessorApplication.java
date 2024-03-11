fd33779e061918c7d15734e01382ce13abf00fe5
Roland Treiber <rolandtreiber@gmail.com>
2024-03-11 02:43:00 +0000

    Initial commit

---
+++ src/main/java/com/techtest/UserDataProcessor/UserDataProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.techtest.UserDataProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDataProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtest/UserDataProcessor/UserDataProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtest.UserDataProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
